fc95d35527cc2e9197a23b1262059e5791b18ee6
dunghenry <trandungksnb6930082001>
2022-09-03 21:27:28 +0700

    basic webpack

---
+++ src/index.js
@@ -0,0 +1,6 @@
+const moment = require('moment');
+// import {sum} from './sum'; //export const sum = (a, b) => a + b;
+const {sum} = require('./sum'); 
+console.log(1);
+console.log(sum(1, 2));
+console.log(moment().format('LT'))
